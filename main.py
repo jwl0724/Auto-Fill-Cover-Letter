e19ef810c83e745c7ba900974fce948c7db3628d
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-07 07:05:05 -0700

    add extra exception checks

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def main():
     except FileNotFoundError:
         print(f"{sys.argv[1]} could not be found, exiting program")
         return
+    except PermissionError:
+        print(f"Program does not have access to {sys.argv[1]}")
+        return
+    except fp.NoMarkedFieldsError:
+        print(f"No marked field founds in the file")
+        return
+    
     parser.set_marked_field("date", get_current_date())
     parser.replace_marked_fields()
     parser.save(f"filled_{sys.argv[1]}")
