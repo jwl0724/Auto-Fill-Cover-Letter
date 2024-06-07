6c8072e6787222908ab2a3d23a2ce64bcc661625
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-07 07:01:14 -0700

    add preservation of paragraph formatting

--- fileParser.py
+++ fileParser.py
@@ -41,11 +41,27 @@ class parser:
 
 
     def replace_marked_fields(self) -> None:
+        bold, font_size, italic, underline, font_family = None, None, None, None, None
         for marked_field in self.marked_fields:
             for paragraph in self.document.paragraphs:
-                if paragraph.text.find(marked_field) == -1:
-                    continue
-                paragraph.text = paragraph.text.replace(marked_field, self.marked_fields[marked_field])
+                for run in paragraph.runs:
+                    # get the most recent run formatting
+                    if bold != run.bold: bold = run.bold
+                    if font_family != run.font.name and run.font.name != None: font_family = run.font.name
+                    if font_size != run.font.size: font_size = run.font.size
+                    if underline != run.underline: underline = run.underline
+                    if italic != run.italic: italic = run.italic
+
+                    if (paragraph.text.find(marked_field) == -1):
+                        continue
+
+                    # apply most recent formatting
+                    paragraph.text = paragraph.text.replace(marked_field, self.marked_fields[marked_field])
+                    paragraph.style.font.size = font_size
+                    paragraph.style.font.underline = underline
+                    paragraph.style.font.bold = bold
+                    paragraph.style.font.name = font_family
+                    paragraph.style.font.italic = italic
 
 
     def save(self, filename: str) -> None:
