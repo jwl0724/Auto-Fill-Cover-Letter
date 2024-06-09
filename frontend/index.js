38a45e26d66905e765d7b81424d9fb4508e53787
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-08 22:02:46 -0700

    move to new folder and seperated file handlers into separate file

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+// MODAL HANDLER
+let modal = document.getElementById("example");
+let showModal = document.getElementById("openExample");
+
+showModal.addEventListener("click", () => {
+    modal.showModal();
+    let closeButton = document.getElementById("closeModal");
+    closeButton.addEventListener("click", () => {
+        modal.close();
+    });
+});
+
